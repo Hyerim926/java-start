c5e7ebea9e40befa129e3ab7612c185685c97bd8
Hyerim926 <ekfrhsk96@icloud.com>
2024-01-28 03:06:07 +0900

    chore: 장바구니 검증 로직 변수화

--- src/access/ex/ShoppingCart.java
+++ src/access/ex/ShoppingCart.java
@@ -5,7 +5,7 @@ public class ShoppingCart {
     private int itemCount = 0;
 
     public void addItem(Item item) {
-        if (itemCount > 10) {
+        if (itemCount > items.length) {
             System.out.println("장바구니가 가득 찼습니다.");
             return;
         }
